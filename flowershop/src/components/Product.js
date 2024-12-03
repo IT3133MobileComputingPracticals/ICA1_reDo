3bd29c54ae075418e5e3f191dc6e0e12283fcc27
Lasantha Kumara <lasanthakumaraofficial@gmail.com>
2024-12-03 18:03:51 +0000

    imported flowers array from flower database

--- flowershop/src/components/Product.js
+++ flowershop/src/components/Product.js
@@ -1,20 +1,29 @@
 import '../assets/CSS/layout.css';
+import {flowers} from './FlowerDB'
+
 export default function Product(){
     
     return(
-        <div className="grid-item">
-
-            <div class="card">
-                <img  />
-                <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
-                    <div class="quantity-container">
-                        <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
+        flowers.map(flower=>
+            <div className="grid-item">
+    
+                <div class="card">
+                    <img  src={require("../assets/image/" + flower.img)}  />
+    
+                    <div class="card-body">
+                        <h5 class="card-title">{flower.name} Price:{flower.price}</h5>
+    
+                        <div class="quantity-container">
+    
+                            <label for="quantity">Quantity:</label>
+    
+                            <input type="number" id="quantity" name="quantity"  onChange={(e)=>setInputValue(e.target.value)}/>
+    
+                        </div>
+                        <button class="card-button" onClick={()=>addCart(flower.name,flower.price,inputValue)}>Add to Cart</button>
                     </div>
-                    <button class="card-button">Add to Cart</button>
                 </div>
             </div>
-        </div>
+            )
     );
 }
