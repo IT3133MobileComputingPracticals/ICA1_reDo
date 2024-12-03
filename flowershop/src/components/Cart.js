21d21691a294c8911e33b120a8da347056bc4bc2
Lasantha Kumara <lasanthakumaraofficial@gmail.com>
2024-12-03 18:25:12 +0000

    mapping getCart

--- flowershop/src/components/Cart.js
+++ flowershop/src/components/Cart.js
@@ -10,21 +10,26 @@ export default function Cart() {
             <h4 className="card-title">Cart</h4>
             <table>
                 <thead>
-
-                    <td>Product</td>
-                    <td>QTY</td>
-                    <td>Price</td>
-
+                    <tr>
+                        <td>Product</td>
+                        <td>QTY</td>
+                        <td>Price</td>
+                    </tr>
                 </thead>
-                {
-                    
-                }
-                <tr>
+                <tbody>
+                    {props.getCart.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.flowerName}</td>
+                            <td>{item.quantity}</td>
+                            <td>{item.quantity*item.flowerPrice}</td>
+                        </tr>
+                    ))}
+                    <tr>
                     <td >Grand Total : </td>
-                    <td colSpan={2}>Grand Total</td>
+                    <td colSpan={2}>{grandTotal}</td>
                 </tr>
+                </tbody>
             </table>
-           
         </div>
     );
 }
